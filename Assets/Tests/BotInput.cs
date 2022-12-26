48b762a96efc5055364f81af2660eb6ff97e64c3
Luqmus <rjcnz1007@yandex.ru>
2022-12-26 11:29:29 +0400

    de

--- Assets/Tests/BotInput.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BotInput : MonoBehaviour, ICharacterInputSource
-{
-    public Vector2 MovementInput { get; set; }
-}
--- Assets/Tests/ICharacterInputSource.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface ICharacterInputSource
-{
-    Vector2 MovementInput { get; }
-}
